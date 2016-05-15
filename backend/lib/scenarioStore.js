386826bcca15d1404dca3bd5578b6c006e560755
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2016-05-15 15:06:22 +0200

    Read playlists and images in order

--- backend/lib/scenarioStore.js
+++ backend/lib/scenarioStore.js
@@ -6,12 +6,11 @@ import { escape } from "querystring";
 import config from "config";
 
 function readImages(scenarioId) {
-    const imageDir = path.join(config.storeDirectory, scenarioId, "images");
+    const imageDir = path.join(config.storeDirectory, scenarioId, "images"),
+          manifestPath = path.join(config.storeDirectory, scenarioId, "images", ".order");
 
-    return Q.nfcall(fs.readdir, imageDir).then(function(entries) {
-        return entries.filter(function(entry) {
-            return entry[0] !== "." && entry !== "thumbnails";
-        });
+    return Q.nfcall(fs.readFile, manifestPath, "utf-8").then(function(imageListJson) {
+        return JSON.parse(imageListJson).images;
     }).then(function(files) {
         return files.map(function(file) {
             return {title: file.replace(/\..*/, ""),
@@ -33,17 +32,18 @@ function readPlaylists(scenarioId) {
         let result = [];
 
         return Q.all(playlists.map(function(playlistName) {
-            return Q.nfcall(fs.readdir, path.join(playlistDir, playlistName)).then(function(entries) {
-                return entries.filter(function(entry) {
-                    return entry[0] !== ".";
-                });
-            });
-        })).then(function(playlistTracks) {
+            return Q.nfcall(
+                fs.readFile,
+                path.join(playlistDir, playlistName, ".order"),
+                "utf-8"
+            );
+        })).then(function(playlistJsonList) {
             playlists.forEach(function(playlistName, i) {
+                const playlistTracks = JSON.parse(playlistJsonList[i]).tracks;
                 result.push({title: playlistName,
-                             tracks: playlistTracks[i].map(function(track) {
+                             tracks: playlistTracks.map(function(trackName) {
                                  return {
-                                     url: "/scenarios/" + scenarioId + "/playlists/" + playlistName + "/" + track
+                                     url: "/scenarios/" + scenarioId + "/playlists/" + playlistName + "/" + trackName
                                  };
                              })});
             });
