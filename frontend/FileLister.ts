5dc43e760490121fc6323ebb99c065bb0a1cdc31
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2016-05-16 09:48:43 +0200

    Add proper scene name formatting

--- frontend/FileLister.ts
+++ frontend/FileLister.ts
@@ -7,6 +7,9 @@ const WEBSOCKET_URL = location.protocol.replace("http", "ws") +
 
 const enum FileListerAppModes { FileList, Map };
 
+@template("/templates/sceneheader.html")
+class SceneHeader extends Riot.Element {}
+
 @template("/templates/filelister.html")
 export default class FileListerApp extends Riot.Element
 {
